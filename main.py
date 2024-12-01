9cf6aaa8fedb4a1ffb0ddcc5f142b25580751afc
salarrbl <rblcrow0101@gmail.com>
2024-12-01 12:26:14 +0330

    updateing

--- main.py
+++ main.py
@@ -1,89 +1,16 @@
 import requests
+import sys
 import random
 from bs4 import BeautifulSoup
 import urllib.parse
 
-# List of Google Dorks
-dorks = [
-    # '"inurl:login"',
-    # '"inurl:admin login"',
-    # '"intitle:"login" inurl:"login""',
-    # '"inurl:"/admin" intitle:"login""',
-    # '"inurl:"login" intext:"username" intext:"password""',
-    # '"inurl:login site:example.com "type="password"""',
-    # '"inurl:wp-login.php"',
-    # '"inurl:administrator login"',
-    # '"inurl:admin/index.php"',
-    # '"inurl:admin intitle:"login""'
-    'inurl:login',
-    'inurl:admin',
-    'inurl:admin login',
-    'inurl:signin',
-    'inurl:signup',
-    'inurl:authentication',
-    'inurl:dashboard',
-    'inurl:cms login',
-    'inurl:portal',
-    'inurl:cpanel',
-    'inurl:staff login',
-    'inurl:user login',
-    'inurl:member login',
-    'inurl:employee login',
-    'inurl:student login',
-    'inurl:teacher login',
-    'inurl:admin area',
-    'inurl:secure login',
-    'inurl:myaccount',
-    'inurl:access',
-    'intitle:"login"',
-    'intitle:"admin login"',
-    'intitle:"sign in"',
-    'intitle:"member login"',
-    'intitle:"employee login"',
-    'intitle:"student login"',
-    'intitle:"teacher login"',
-    'intitle:"portal login"',
-    'intitle:"admin panel"',
-    'intitle:"control panel"',
-    'intitle:"cpanel login"',
-    'intitle:"secure login"',
-    'intitle:"authentication required"',
-    'intitle:"account login"',
-    'intitle:"access control"',
-    'intitle:"admin area"',
-    'intitle:"user authentication"',
-    'intitle:"sign in to your account"',
-    'intitle:"portal"',
-    'intitle:"dashboard"'
-]
-user_agent = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.100 Safari/537.36'
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.100 Safari/537.36'
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:112.0) Gecko/20100101 Firefox/112.0'
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:112.0) Gecko/20100101 Firefox/112.0'
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.100 Safari/537.36 Edge/110.0.1587.57'
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0.3 Safari/537.36'
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.6 Mobile/15E148 Safari/604.1'
-    'Mozilla/5.0 (iPad; CPU OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.6 Mobile/15E148 Safari/604.1'
-    'Mozilla/5.0 (Linux; Android 10; Pixel 4 XL Build/QD1A.200205.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.136 Mobile Safari/537.36'
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/70.0.3728'
-    'Mozilla/5.0 (Windows NT 10.0; Trident/7.0; AS; rv:11.0) like Gecko'
-    'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-    'Mozilla/5.0 (compatible; Bingbot/2.0; +http://www.bing.com/bingbot.htm)'
-    'Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)'
-    'Mozilla/5.0 (compatible; DuckDuckBot/1.0; +https://duckduckgo.com/duckduckbot)'
-    'Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)'
-    'Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)'
-    'Mozilla/5.0 (Linux; Android 9; Pixel 2 Build/PQ3A.190801.002) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Mobile Safari/537.36 TelegramBot/5.15'
+with open(sys.argv[1], 'r') as file:
+    dorks = file.readlines()
 
-]
 def search_google(dork,start):
     query = urllib.parse.quote(dork)
     url = f"https://www.google.com/search?q={query}&start={start}"
-    random_user_agent = random.choice(user_agent)
-    headers = {"User-Agent": random_user_agent}
-
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     # print(response.status_code)
     # Extract URLs from search results
